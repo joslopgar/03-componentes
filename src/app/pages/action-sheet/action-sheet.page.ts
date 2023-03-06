caaaca0fe590656e4dc7b7dec660486492a67543
Jose Lopez Garcia <jlgarcia92@gmail.com>
2023-03-06 22:29:32 +0100

    Start of Lecture 42

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
   selector: 'app-action-sheet',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./action-sheet.page.scss'],
 })
 export class ActionSheetPage implements OnInit {
+  result: string | undefined;
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
   }
 
+  onClick() {
+    this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Albumes',
+      backdropDismiss: false,
+      // subHeader: 'Example subheader',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash-outline',
+          cssClass: 'rojo',
+          handler: () => {
+            console.log('Deleted!')
+          },
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share with...',
+          icon: 'share-social-outline',
+          cssClass: 'green-share-ios',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+
+    const result = await actionSheet.onDidDismiss();
+    this.result = JSON.stringify(result, null, 2);
+  }
 }
